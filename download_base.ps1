f33860d27ad12f69f215807025146615d4985177
CaesarRulezzz <caesarrulezzz@gmail.com>
2018-04-14 09:34:28 +0300

    A little bit of optimization
    1) the script is using now system's temp directory instead of the hardcoded one
    2) I've added if condition for checking if the target path is avaliable

--- download_base.ps1
+++ download_base.ps1
@@ -1,5 +1,8 @@
 $scriptDriveLetter = (Get-Volume -FileSystemLabel REGIST).DriveLetter
 $outputPath = "${scriptDriveLetter}:\"
-wget https://trend-vision.ru/media/files/simple/productfile/FULL_Base.rar -OutFile "D:\Full_base.rar"
-& "C:\Program Files\Easy 7-Zip\7z" e "D:\Full_Base.rar" -o"${outputPath}" -y
-Remove-Item D:\Full_Base.rar -recurse
+if ($scriptDriveLetter.length -gt 0)
+{
+    wget https://trend-vision.ru/media/files/simple/productfile/FULL_Base.rar -OutFile ${env:TEMP}"\Full_Base.rar"
+    & "C:\Program Files\Easy 7-Zip\7z" e ${env:TEMP}"\Full_Base.rar" -o"${outputPath}" -y
+    Remove-Item ${env:TEMP}\Full_Base.rar -recurse
+}
