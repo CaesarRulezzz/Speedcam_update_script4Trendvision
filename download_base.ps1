61eb406c932919682e8f5f8dab72109293925075
CaesarRulezzz <caesarrulezzz@gmail.com>
2019-06-24 20:49:52 +0300

    Added support for databases for Show-Me
    Added support for databases for Show-Me.
    Pass trendvision or showme as parameter

--- download_base.ps1
+++ download_base.ps1
@@ -1,8 +1,31 @@
+$type = $args[0]
+
+if ($type -eq "trendvision")
+{
+    # trendvision
+    $filename = "Full_Base.rar"
+    $download_link = "https://trend-vision.ru/media/files/simple/productfile/FULL_Base.rar"
+}
+elseif ($type -eq "showme")
+{
+    # showme
+    $download_link = "https://speedcam.online/rd.online/shome/combo7new/combo_database.zip"
+    $filename = "Full_Base.zip"
+}
+else {
+    Write-Output "No parameter!"
+    Exit-PSSession
+}
+
+$7zip_path = "C:\Program Files\Easy 7-Zip\7z"
 $scriptDriveLetter = (Get-Volume -FileSystemLabel REGIST).DriveLetter
 $outputPath = "${scriptDriveLetter}:\"
 if ($scriptDriveLetter.length -gt 0)
 {
-    wget https://trend-vision.ru/media/files/simple/productfile/FULL_Base.rar -OutFile ${env:TEMP}"\Full_Base.rar"
-    & "C:\Program Files\Easy 7-Zip\7z" e ${env:TEMP}"\Full_Base.rar" -o"${outputPath}" -y
-    Remove-Item ${env:TEMP}\Full_Base.rar -recurse
+    Invoke-WebRequest $download_link -OutFile ${env:TEMP}"\"$filename
+    & $7zip_path e ${env:TEMP}"\"$filename -o"${outputPath}" -y
+    Remove-Item ${env:TEMP}\$filename -recurse
+}
+else {
+    Write-Output "No drive found!"
 }
